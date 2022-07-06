f93d0303de176768127ed9fd7144d141283332e8
Jihunp <edwardpark.park@gmail.com>
2022-07-05 21:27:08 -0700

    starting files for project completed and minimal navbar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ALogo from "../images/ALogo.png"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={ALogo} alt="airbnb logo" className="nav--logo"/>
+        </nav>
+    )
+}
+
+// 2:34:34
